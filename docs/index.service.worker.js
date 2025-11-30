503e9f2e31a7ca91ea21d0c17242c364b1ef998f
Lauro C Oliveira <lauro@hostcert.com.br>
2025-11-30 01:45:26 -0300

    add logo

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764477812|3076241';
+const CACHE_VERSION = '1764477917|3308643';
 /** @type {string} */
 const CACHE_PREFIX = 'IMPOSTEIRO-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
