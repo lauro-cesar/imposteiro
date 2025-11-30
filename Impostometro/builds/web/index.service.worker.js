78d4126cf09df06ef7edad0fa66ce19fb212d3c5
Lauro C Oliveira <lauro@hostcert.com.br>
2025-11-30 02:23:30 -0300

    Removido progressbar

--- Impostometro/builds/web/index.service.worker.js
+++ Impostometro/builds/web/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1764479737|3198250';
+const CACHE_VERSION = '1764480196|3384240';
 /** @type {string} */
 const CACHE_PREFIX = 'IMPOSTEIRO-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
